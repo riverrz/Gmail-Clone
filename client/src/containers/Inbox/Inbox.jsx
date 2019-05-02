4e101ee0fd0d27a4a5ec572c375d29dab347098c
shivam123425 <shivam123425@gmail.com>
2019-05-02 21:59:23 +0530

    Add temp fields and styled Inbox

--- client/src/containers/Inbox/Inbox.jsx
+++ client/src/containers/Inbox/Inbox.jsx
@@ -1,14 +1,9 @@
 import React, { Component, Fragment } from "react";
 import { connect } from "react-redux";
 import * as actionTypes from "../../store/actions/actionTypes";
-import Profile from "./Profile/Profile";
 import "./Inbox.css";
-import qs from "qs";
 
 class Inbox extends Component {
-  state = {
-    tabs: ["Inbox", "Current Mentors", "Profile", "Find Mentors"]
-  };
   handleLogout = () => {
     // Remove the token from local storage
     localStorage.removeItem("token");
@@ -16,48 +11,35 @@ class Inbox extends Component {
     this.props.logout();
   };
   render() {
-    const query = qs.parse(this.props.location.search, {
-      ignoreQueryPrefix: true
-    });
-    const iconClass = [
-      "fas fa-chart-pie",
-      "fas fa-user",
-      "far fa-id-card",
-      "fas fa-search"
-    ];
-    let heading = "Inbox";
-    if (this.state.tabs.includes(query.tab)) {
-      heading = query.tab;
-    }
-    let listItems = this.state.tabs.map((item, i) => {
-      const listItemClasses = ["Inbox__sideList__item"];
-      if (query.tab === item) {
-        listItemClasses.push("Inbox__sideList__item--active");
-      }
-      return (
-        <li key={i} className={listItemClasses.join(" ")}>
-          <i className={iconClass[i]} />
-          {item}
-        </li>
-      );
-    });
-
-    let content = <Profile />;
     return (
       <Fragment>
         <nav className="Inbox__nav">
-          {heading}
+          Gmail Clone
           <button onClick={this.handleLogout} className="btn btn-logout">
             Logout
           </button>
         </nav>
         <main className="Inbox__main">
-          <aside className="Inbox__sideDrawer">
-            <h3>Student Mentor</h3>
-            <hr />
-            <ul className="Inbox__sideList">{listItems}</ul>
-          </aside>
-          <section className="Inbox__section">{content}</section>
+          <ul className="Inbox__mailList">
+            <li className="Inbox__mailList__item Inbox__heading">
+              <p>From</p>
+              <p>Subject</p>
+              <p>Description</p>
+              <p>Timestamp</p>
+            </li>
+            <li className="Inbox__mailList__item">
+              <p>Shivam Kumar</p>
+              <p>Hello world</p>
+              <p>This is the description</p>
+              <p>12/03/19</p>
+            </li>
+            <li className="Inbox__mailList__item">
+              <p>Shivam Kumar</p>
+              <p>Hello world</p>
+              <p>This is the description</p>
+              <p>12/03/19</p>
+            </li>
+          </ul>
         </main>
       </Fragment>
     );
